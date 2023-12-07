746dbd53bffbc73f4136b38b3ac563d828a3aa85
Tim Bartsch <tim-bartsch@outlook.com>
2023-12-07 22:01:08 +0100

    Changes to be committed: 	modified:   Youtube/index.html 	modified:   Youtube/index.py 	new file:   Youtube/pyscript.json

--- Youtube/index.py
+++ Youtube/index.py
@@ -1,32 +1,39 @@
-from flask import *
+#from flask import render_template, send_file, Flask
 from pytube import YouTube
+from pyscript import *
 
-app = Flask(__name__)  
-
-@app.route('/Youtube', methods=['GET', 'POST'])
-def Youtube_Downloaderd():
-    URL = request.form.get("youtube-url")
-    Format = request.form.get("File")
-    print(URL)
-    print(Format)
-    try:
-        yt = YouTube(URL)
+#app = Flask(__name__)
+def Youtube_Downloader():
+#    URL = request.form.get("youtube-url")
+#    Format = request.form.get("File")
+#    print(URL)
+#    print(Format)
+    #try:
+        URL = "https://www.youtube.com/watch?v=OUoeZTJzcyY"
+        Format = "MP4"
+        yt = YouTube(str(URL))
         audio = yt.streams.filter(only_audio = True).first()
         yd = yt.streams.get_highest_resolution()
+        print(URL)
+        print(Format)
         if (Format == "MP4"):
-            yd.download("X:/Dateien", filename=f"{yt.title}.mp4")
+            yd.download("./Dateien", filename=f"{yt.title}.mp4")
             name=yt.title+".mp4"
+            print(name)
         else:
-            audio.download("X:/Dateien", filename=f"{yt.title}.mp3")
+            audio.download("./Dateien", filename=f"{yt.title}.mp3")
             name=yt.title+".mp3"
-    except:
-        URL="https://www.youtube.com/watch?v=qnOySOYwmAY"
-        Format="MP4" 
-    try:
-        return redirect("/Youtube-Downloader/"+name)
-    except:
-        return render_template('Youtube-Downloader.html')
+            print(name)
+    #except:
+        print("fail")
+#    try:
+#        return redirect("/Youtube-Downloader/"+name)
+#        return send_file("./Dateien/"+name, as_attachment=True)
+#    except:
+#        return render_template('./index.html')
+    
 
-@app.route('/Youtube/<name>')
-def Youtube_Downloader_download(name):
-    return send_file("X:/Dateien/"+name, as_attachment=True)
+
+#@app.route('/Youtube/<name>')
+#def Youtube_Downloader_download(name):
+    
