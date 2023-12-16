dc521943880f91f50bd2c21c03b894949325c6c0
Mr.Stinktier <tim-bartsch@outlook.com>
2023-12-16 16:51:47 +0100

    Zum Commit vorgemerkte Änderungen: 	neue Datei:     .gitignore 	neue Datei:     Youtube/Server/Server.js 	geändert:       Youtube/index.html 	neue Datei:     Youtube/index.js 	geändert:       package-lock.json 	geändert:       package.json

---
+++ Youtube/index.js
@@ -0,0 +1,40 @@
+let convertBtn = document.getElementById('submitbutton');
+let URLinput = document.querySelector('.youtube-url');
+let select = document.querySelector('.youtube-url');
+let serverURL = 'http://localhost:4000'
+
+Btn.addEventListener('click', () => {
+	if (!URLinput.value) {
+		alert('Enter YouTube URL');
+	} else {
+		if (select.value == 'MP3') {
+			downloadMp3(URLinput.value);
+		} else if (select.value == 'MP4') {
+			downloadMp4(URLinput.value);
+		}
+	}
+});
+
+async function downloadMp3(query) {
+	const res = await fetch(`${serverURL}/downloadmp3?url=${query}`);
+	if(res.status == 200) {
+		var a = document.createElement('a');
+  		a.href = `${serverURL}/downloadmp3?url=${query}`;
+  		a.setAttribute('download', '');
+		a.click();
+	} else if(res.status == 400) {
+		alert("Invalid url");
+	}
+}
+
+async function downloadMp4(query) {
+	const res = await fetch(`${serverURL}/downloadmp4?url=${query}`);
+	if(res.status == 200) {
+		var a = document.createElement('a');
+  		a.href = `${serverURL}/downloadmp4?url=${query}`;
+  		a.setAttribute('download', '');
+		a.click();
+	} else if(res.status == 400) {
+		alert('Invalid url');
+	}
+}
