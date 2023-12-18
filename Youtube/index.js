1f144e2e452f22c473cc65853924c5197aa9d1aa
MrStinktier <144330316+MrStinktier@users.noreply.github.com>
2023-12-18 14:07:58 +0000

    Changes to be committed: 	modified:   Youtube/index.js

--- Youtube/index.js
+++ Youtube/index.js
@@ -1,7 +1,11 @@
 let convertBtn = document.getElementById('submitButtonid');
 let URLinput = document.querySelector('.youtube-url');
 let select = document.querySelector('.File');
-let serverURL = 'https://fuzzy-space-disco-979xp7rwxxrv277xg-4000.app.github.dev'
+let serverURL = 'https://fuzzy-space-disco-979xp7rwxxrv277xg-4000.app.github.dev';
+const times = "10";
+const delay = "10"
+
+window.addEventListener('online', chk)
 
 convertBtn.addEventListener('click', () => {
 	if (!URLinput.value) {
@@ -45,4 +49,25 @@ async function downloadMp4(query) {
 	//} else if(res.status == 400) {
 	//	alert('Invalid url');
 	//}
+}
+
+async function chk(event, serverURL, times, delay) {
+    return new Promise((res, rej) => {                       // return a promise
+
+        (function rec(i) {                                   // recursive IIFE
+            fetch(serverURL, {mode: 'no-cors'}).then((r) => {   // fetch the resourse
+                res(r);   
+				var a = document.createElement('a');
+  				a.href = `www.google.com`;
+  				a.setAttribute('link', '');
+				a.click();                                   // resolve promise if success
+            }).catch( err => {
+                if (times === 0)                             // if number of tries reached
+                    return rej(err);                         // don't try again
+
+                setTimeout(() => rec(--times), delay )       // otherwise, wait and try 
+            });                                              // again until no more tries
+        })(times);
+
+    });
 }
