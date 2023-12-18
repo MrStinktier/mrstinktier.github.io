b62c33f2b07aa105f81322deb1633641ace67895
MrStinktier <144330316+MrStinktier@users.noreply.github.com>
2023-12-18 15:00:46 +0000

    Changes to be committed: 	modified:   Youtube/index.js 	modified:   index.html

--- Youtube/index.js
+++ Youtube/index.js
@@ -51,23 +51,17 @@ async function downloadMp4(query) {
 	//}
 }
 
-async function chk(event, serverURL, times, delay) {
-    return new Promise((res, rej) => {                       // return a promise
+const checkOnlineStatus = async () => {
+	try {
+	  const online = await fetch("https://fuzzy-space-disco-979xp7rwxxrv277xg-4000.app.github.dev");
+	  return online.status >= 200 && online.status < 300; // either true or false
+	} catch (err) {
+	  return false; // definitely offline
+	}
+};
 
-        (function rec(i) {                                   // recursive IIFE
-            fetch(serverURL, {mode: 'no-cors'}).then((r) => {   // fetch the resourse
-                res(r);   
-				var a = document.createElement('a');
-  				a.href = `www.google.com`;
-  				a.setAttribute('link', '');
-				a.click();                                   // resolve promise if success
-            }).catch( err => {
-                if (times === 0)                             // if number of tries reached
-                    return rej(err);                         // don't try again
-
-                setTimeout(() => rec(--times), delay )       // otherwise, wait and try 
-            });                                              // again until no more tries
-        })(times);
-
-    });
-}
+setInterval(async () => {
+	const result = await checkOnlineStatus();
+	const statusDisplay = document.getElementById("status");
+	statusDisplay.textContent = result ? "Online" : "OFFline";
+}, 3000); // probably too often, try 30000 for every 30 seconds
