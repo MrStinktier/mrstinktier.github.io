59ba8291ab0c7fa95268a627ddccb7c2d8c05987
Mr.Stinktier <tim-bartsch@outlook.com>
2023-12-27 21:42:34 +0100

    Zum Commit vorgemerkte Änderungen: 	geändert:       Jeopardy/index.js

--- Jeopardy/index.js
+++ Jeopardy/index.js
@@ -5,33 +5,33 @@ let name4_link = document.getElementById("0,4");
 let name5_link = document.getElementById("0,5");
 
 let name1_100_link = document.getElementById("1,1");
-let name1_200_link = document.getElementById("1,2");
-let name1_300_link = document.getElementById("1,3");
-let name1_400_link = document.getElementById("1,4");
-let name1_500_link = document.getElementById("1,5");
+let name2_100_link = document.getElementById("1,2");
+let name3_100_link = document.getElementById("1,3");
+let name4_100_link = document.getElementById("1,4");
+let name5_100_link = document.getElementById("1,5");
 
-let name2_100_link = document.getElementById("2,1");
+let name1_200_link = document.getElementById("2,1");
 let name2_200_link = document.getElementById("2,2");
-let name2_300_link = document.getElementById("2,3");
-let name2_400_link = document.getElementById("2,4");
-let name2_500_link = document.getElementById("2,5");
+let name3_200_link = document.getElementById("2,3");
+let name4_200_link = document.getElementById("2,4");
+let name5_200_link = document.getElementById("2,5");
 
-let name3_100_link = document.getElementById("3,1");
-let name3_200_link = document.getElementById("3,2");
+let name1_300_link = document.getElementById("3,1");
+let name2_300_link = document.getElementById("3,2");
 let name3_300_link = document.getElementById("3,3");
-let name3_400_link = document.getElementById("3,4");
-let name3_500_link = document.getElementById("3,5");
+let name4_300_link = document.getElementById("3,4");
+let name5_300_link = document.getElementById("3,5");
 
-let name4_100_link = document.getElementById("4,1");
-let name4_200_link = document.getElementById("4,2");
-let name4_300_link = document.getElementById("4,3");
+let name1_400_link = document.getElementById("4,1");
+let name2_400_link = document.getElementById("4,2");
+let name3_400_link = document.getElementById("4,3");
 let name4_400_link = document.getElementById("4,4");
-let name4_500_link = document.getElementById("4,5");
+let name5_400_link = document.getElementById("4,5");
 
-let name5_100_link = document.getElementById("5,1");
-let name5_200_link = document.getElementById("5,2");
-let name5_300_link = document.getElementById("5,3");
-let name5_400_link = document.getElementById("5,4");
+let name1_500_link = document.getElementById("5,1");
+let name2_500_link = document.getElementById("5,2");
+let name3_500_link = document.getElementById("5,3");
+let name4_500_link = document.getElementById("5,4");
 let name5_500_link = document.getElementById("5,5");
 
 name1_100_link.addEventListener('click', () => {
