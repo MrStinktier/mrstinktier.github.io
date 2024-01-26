af8bff72017ba275b0dafd4cbd283170c7ec4529
Tim Bartsch <tim-bartsch@outlook.com>
2024-01-26 17:06:19 +0100

    Zum Commit vorgemerkte Änderungen: 	geändert:       Wakeup/index.js

--- Wakeup/index.js
+++ Wakeup/index.js
@@ -8,8 +8,8 @@ async function wakeup() {
     const res = await fetch(`https://backend.mr-stinktier.uk/Wakeup`);
 	if(res.status == 200) {
         console.log("Worked");
+        location.replace("https://web.parsec.app");
     }else if(res.status == 400) {
         console.log("Something went wrong");
     }
-    return "https://web.parsec.app"
 }
