865be555128c6c5b4e7d571a5c1e2ccb23931f0e
Tim Bartsch <tim-bartsch@outlook.com>
2024-02-05 16:50:44 +0100

    Changes to be committed: 	modified:   Wakeup/index.js

--- Wakeup/index.js
+++ Wakeup/index.js
@@ -5,12 +5,8 @@ button.addEventListener('click', () => {
 });
 
 async function wakeup() {
-<<<<<<< HEAD
-    const res = await fetch(`https://backend.mr-stinktier.uk/Wakeup`/*`https://backend.mr-stinktier.uk/Wakeup?url=${query}`*/);
-=======
     var mac = "00:13:3b:0c:64:3f"
     const res = await fetch(`https://backend.mr-stinktier.uk/start?mac=${mac}`);
->>>>>>> 3c2de375004b2b31f0f67043d92a7efb3a61ad8d
 	if(res.status == 200) {
         console.log("Worked");
         location.replacer("https://web.parsec.app");
