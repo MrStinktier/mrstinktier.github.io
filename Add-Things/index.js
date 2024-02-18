0059558eca2793ed5eafdfeedf417a734741b5a5
Tim Bartsch <tim-bartsch@outlook.com>
2024-02-18 11:46:07 +0100

    Zum Commit vorgemerkte Änderungen: 	geändert:       Add-Things/index.css 	geändert:       Add-Things/index.html 	geändert:       Add-Things/index.js

--- Add-Things/index.js
+++ Add-Things/index.js
@@ -1,6 +1,16 @@
 let Add_Button = document.getElementById("Add-Button");
 let adder = document.getElementById("adder");
+let popupWindow = document.getElementById("popup-div");
+let popupButton = document.getElementById("popup-button");
 
 Add_Button.addEventListener("click", function(event){
-    adder.innerHTML += '<p>A new paragraph</p>';
+    event.preventDefault();
+    popupWindow.style.display = "block";
 });
+
+popupButton.addEventListener("click", function(event){
+    event.preventDefault();
+    popupWindow.style.display = "none";
+});
+
+//adder.innerHTML += "hello"
