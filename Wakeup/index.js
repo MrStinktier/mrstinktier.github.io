c1271497af781c06fd7f44a5fc1c524b82f09e3a
Tim Bartsch <tim-bartsch@outlook.com>
2024-06-03 17:43:29 +0200

    Changes to be committed: 	modified:   Wakeup/index.css 	modified:   Wakeup/index.html 	modified:   Wakeup/index.js

--- Wakeup/index.js
+++ Wakeup/index.js
@@ -1,3 +1,5 @@
+var statdropdown = "disabled";
+
 document.addEventListener("click", function(event) {
 	switch (event.target.id) {
 	  	case "button":
@@ -5,6 +7,21 @@ document.addEventListener("click", function(event) {
 			break;
 	  	case "button2":
 			wakeup("a0:b3:cc:eb:0d:d3");
+			break;
+		case "shutdown":
+		  	Shutdown("shutdown");
+		  	break;
+		case "reboot":
+		  	Shutdown("reboot");
+		  	break;
+		case "shutdown-dropdown":
+			if(statdropdown=="disabled"){
+				this.getElementById("sh-dropdown-contents").style.display = "flex";
+				statdropdown = "enabled";
+			}else if(statdropdown=="enabled"){
+				this.getElementById("sh-dropdown-contents").style.display = "none";
+				statdropdown = "disabled";
+			}
 			break;
 	  	default:
 			break;
@@ -14,6 +31,16 @@ document.addEventListener("click", function(event) {
 async function wakeup(mac) {
     const res = await fetch(`https://backend.mr-stinktier.uk/start?mac=${mac}`);
     console.log(mac);
+	if(res.status == 200) {
+        console.log("Worked");
+    }else if(res.status == 400) {
+        console.log("Something went wrong");
+    }
+}
+
+async function Shutdown(status) {
+    const res = await fetch(`https://backend.mr-stinktier.uk/shutdown?stat=${status}`);
+    console.log(status);
 	if(res.status == 200) {
         console.log("Worked");
     }else if(res.status == 400) {
